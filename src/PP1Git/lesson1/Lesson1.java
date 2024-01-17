7a718c32ebc32cac5e66ade7072375eea0f9e5d2
Maksim M0rokov <maxmorokov7@gmail.com>
2024-01-17 16:07:47 +0300

    my commit1

---
+++ src/PP1Git/lesson1/Lesson1.java
@@ -0,0 +1,12 @@
+package PP1Git.lesson1;
+
+public class Lesson1 {
+    public static void main(String[] args) {
+
+        task1();
+    }
+    private static void task1() {
+        System.out.println("решение задачи 1");
+    }
+
+}
