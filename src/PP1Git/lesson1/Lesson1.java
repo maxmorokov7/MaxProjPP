4a161f6b36bc44d80ea3fdcfb14b8ce3d7ea9d55
Maksim M0rokov <maxmorokov7@gmail.com>
2024-01-17 16:48:50 +0300

    my commit1(change1)

--- src/PP1Git/lesson1/Lesson1.java
+++ src/PP1Git/lesson1/Lesson1.java
@@ -4,6 +4,7 @@ public class Lesson1 {
     public static void main(String[] args) {
 
         task1();
+        System.out.println("добавил изменение 1");
     }
     private static void task1() {
         System.out.println("решение задачи 1");
